4a78eb19510f4eacfadc152ed1d2bae65cd3aaad
Renaud Allard <renaud@allard.it>
2012-01-10 15:26:27 +0100

    Move config at the right place :)

--- repack_bootimg.sh
+++ repack_bootimg.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-cd initramfs-*
- find . | cpio -o -H newc | gzip > ../new_initramfs.cpio.gz
-cd ..
-./mkbootimg --cmdline 'console=ttyO2,115200n8 androidboot.console=ttyO2' --kernel omap/arch/arm/boot/zImage --ramdisk new_initramfs.cpio.gz -o kernel.img
+for RAMV in 4.0.2 4.0.3-cm9
+do
+	cd initramfs-${RAMV}
+	find . | cpio -o -H newc | gzip > ../new_initramfs.cpio.gz
+	cd ..
+	./mkbootimg --kernel omap/arch/arm/boot/zImage --ramdisk new_initramfs.cpio.gz -o ../public_html/galaxy_nexus/kernel-${RAMV}.img 
+done
+
+
+rm new_initramfs.cpio.gz
