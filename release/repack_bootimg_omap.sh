25698cb0f474b70e36397b2bb4f952b2b67db7c5
r <r@panda.localdomain>
2012-02-06 12:20:24 +0100

    New repack script for omap board

---
+++ release/repack_bootimg_omap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+RAMV=$1
+RELV="`cat ../.version`-${RAMV}"
+RELV="_FuguMod_$(date +%Y%m%d_r)${RELV}"
+if [ "ZZ${RAMV}" == "ZZ" ]
+then
+	echo "No version specified"
+	exit 1
+else
+	cd initramfs-${RAMV} || exit 1
+	find . | cpio -o -H newc | gzip > ../new_initramfs.cpio.gz
+	cd ..
+	abootimg --create /ext_usb/kernel${RELV}.img -k ../arch/arm/boot/zImage -r new_initramfs.cpio.gz
+	sha256sum /ext_usb//kernel${RELV}.img > /ext_usb/kernel${RELV}.sha256sum
+
+	cd cwm
+	cp /ext_usb/kernel${RELV}.img boot.img
+        zip -q -r /ext_usb/kernel${RELV}.zip boot.img META-INF || exit 1
+        sha256sum /ext_usb/kernel${RELV}.zip > /ext_usb/kernel${RELV}.zip.sha256sum
+
+	scp /ext_usb/kernel* faramir.eriador.org:public_html/galaxy_nexus/testing-${RAMV}/
+
+	cd ..
+	rm new_initramfs.cpio.gz cwm/boot.img /ext_usb/kernel*
+fi
+
+
