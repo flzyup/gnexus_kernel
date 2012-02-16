5206995d78a1640b4c6d8718f2cfb6f9e68beeff
Renaud Allard <renaud@allard.it>
2012-02-16 13:59:02 +0100

    Change paths in repacking script

--- release/repack_bootimg_omap.sh
+++ release/repack_bootimg_omap.sh
@@ -11,20 +11,20 @@ else
 	cd initramfs-${RAMV} || exit 1
 	find . | cpio -o -H newc | gzip > ../new_initramfs.cpio.gz
 	cd ..
-	abootimg --create /ext_usb/kernel${RELV}.img -k ../arch/arm/boot/zImage -r new_initramfs.cpio.gz
-	sha256sum /ext_usb//kernel${RELV}.img > /ext_usb/kernel${RELV}.sha256sum
+	abootimg --create /devel/kernel${RELV}.img -k ../arch/arm/boot/zImage -r new_initramfs.cpio.gz
+	sha256sum /devel/kernel${RELV}.img > /ext_usb/kernel${RELV}.sha256sum
 
 	git tag -a r${KVER} -m r${KVER}
 
 	cd cwm
-	cp /ext_usb/kernel${RELV}.img boot.img
-        zip -q -r /ext_usb/kernel${RELV}.zip boot.img META-INF || exit 1
-        sha256sum /ext_usb/kernel${RELV}.zip > /ext_usb/kernel${RELV}.zip.sha256sum
+	cp /devel/kernel${RELV}.img boot.img
+        zip -q -r /devel/kernel${RELV}.zip boot.img META-INF || exit 1
+        sha256sum /devel/kernel${RELV}.zip > /ext_usb/kernel${RELV}.zip.sha256sum
 
-	scp /ext_usb/kernel* faramir.eriador.org:public_html/galaxy_nexus/testing-${RAMV}/
+	scp /devel/kernel* faramir.eriador.org:public_html/galaxy_nexus/testing-${RAMV}/
 
 	cd ..
-	rm new_initramfs.cpio.gz cwm/boot.img /ext_usb/kernel*
+	rm new_initramfs.cpio.gz cwm/boot.img /devel/kernel*
 	
 fi
 
