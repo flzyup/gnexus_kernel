877ecd2a2f06a40ba557a728a48f53f9590949f8
Renaud Allard <renaud@allard.it>
2012-07-13 08:11:29 +0200

    Change repack script to a more generic version

--- release/repack_bootimg.sh
+++ release/repack_bootimg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 RAMV=$1
+KVER=`cat ../.version`
 RELV="`cat ../.version`-${RAMV}"
 RELV="_FuguMod_$(date +%Y%m%d_r)${RELV}"
 if [ "ZZ${RAMV}" == "ZZ" ]
@@ -10,17 +11,19 @@ else
 	cd initramfs-${RAMV} || exit 1
 	find . | cpio -o -H newc | gzip > ../new_initramfs.cpio.gz
 	cd ..
-	./mkbootimg --kernel ../arch/arm/boot/zImage --ramdisk new_initramfs.cpio.gz -o ../../public_html/galaxy_nexus/kernel${RELV}.img 
-	sha256sum ../../public_html/galaxy_nexus/kernel${RELV}.img > ../../public_html/galaxy_nexus/kernel${RELV}.sha256sum
+	abootimg --create $HOME/kernel${RELV}.img -k ../arch/arm/boot/zImage -r new_initramfs.cpio.gz
+
+	git tag -a r${KVER} -m r${KVER}
 
 	cd cwm
-	cp ../../../public_html/galaxy_nexus/kernel${RELV}.img boot.img
-        zip -q -r ../../../public_html/galaxy_nexus/kernel${RELV}.zip boot.img META-INF || exit 1
-        sha256sum ../../../public_html/galaxy_nexus/kernel${RELV}.zip > ../../../public_html/galaxy_nexus/kernel${RELV}.zip.sha256sum
+	mv $HOME/kernel${RELV}.img boot.img
+        zip -q -r $HOME/kernel${RELV}.zip boot.img META-INF || exit 1
+        sha256sum $HOME/kernel${RELV}.zip > $HOME/kernel${RELV}.zip.sha256sum
+
+	scp $HOME/kernel* elendil.arnor.org:public_html/galaxy_nexus/testing-${RAMV}/
 
 	cd ..
-	rm new_initramfs.cpio.gz cwm/boot.img
-
+	rm new_initramfs.cpio.gz cwm/boot.img $HOME/kernel*
+	
 fi
-
 
